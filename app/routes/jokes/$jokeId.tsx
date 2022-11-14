59d71dd870662070dc17809d22b297bdf673f492
Miguel Prada <mapra99@gmail.com>
2022-11-14 17:22:45 -0500

    Base routes and styles

---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,9 @@
+export default () => (
+  <div>
+    <p>Here's your hilarious joke:</p>
+    <p>
+      Why don't you find hippopotamuses hiding in trees?
+      They're really good at it.
+    </p>
+  </div>
+)
